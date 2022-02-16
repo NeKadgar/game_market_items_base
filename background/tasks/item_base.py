fa3811c2cd73a4df5c4ea5229d6ac7f56267ab5e
siarhein2022 <siarhei.naralenkau@itechart-group.com>
2022-02-16 10:09:59 +0300

    Test commit of git local

--- background/tasks/item_base.py
+++ background/tasks/item_base.py
@@ -4,5 +4,6 @@ from services.csmoney import DotaDataProcessor
 
 @app.task(name="update_items_base", queue="items_base_queue")
 def update_items_base(**kwargs):
+    # test commit
     DotaDataProcessor.update(kwargs)
     return kwargs
